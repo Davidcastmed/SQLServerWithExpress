c7d7851308377207254b8ca4d18e439f16a63eac
Davidcastmed <castillo.medina.david@gmail.com>
2025-02-06 09:45:23 +0100

    envTestFile

---
+++ utils/env.js
@@ -0,0 +1,6 @@
+// Este archivo no es valido... ayuda a saber los archivos contenidos en .env en este ejemplo solo lo ultilizo como recordatorio
+
+DB_USER = 'UserDB1'
+DB_PASSWORD = '123456'
+DB_SERVER = 'localhost'
+DB_DATABASE = 'PHI'
